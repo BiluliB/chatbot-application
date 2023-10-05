7435a82249e3640965ca74d49d8b6d7ef1a71971
mahgoe <bilaliwork@gmail.com>
2023-10-05 17:05:57 +0200

    add gui, add botengine

---
+++ Common/BotEngine.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace chatbot_application
+{
+    public class BotEngine
+    {
+        private readonly string apiKey;
+        private bool isWaitingForLocation = false;
+
+        public BotEngine(string apiKey)
+        {
+            this.apiKey = apiKey;
+        }
+
+        public bool IsWaitingForLocation
+        {
+            get { return isWaitingForLocation; }
+            set { isWaitingForLocation = value; }
+        }
+
+        /// <summary>
+        /// Generates a bot response based on user input.
+        /// </summary>
+        /// <param name="userInput">The user input.</param>
+        /// <returns>The bot response.</returns>
+        public string GetBotResponse(string userInput)
+        {
+            userInput = userInput.ToLowerInvariant();
+
+            if (userInput.Contains("hallo") || userInput.Contains("hi") || userInput.Contains("hey"))
+                return "Hallo! Wie kann ich Ihnen heute helfen?";
+
+            if (userInput.Contains("tschüss") || userInput.Contains("ciao") || userInput.Contains("auf wiedersehen"))
+                return "Auf Wiedersehen! Wenn Sie weitere Fragen haben, stehe ich Ihnen gerne zur Verfügung.";
+
+            if (userInput.Contains("wetter"))
+            {
+                isWaitingForLocation = true;
+                return "Für welchen Ort möchten Sie das Wetter wissen?";
+            }
+
+            if (userInput.Contains("uhrzeit") || userInput.Contains("zeit"))
+                return $"Die aktuelle Uhrzeit ist {DateTime.Now:HH:mm}.";
+
+            if (userInput.Contains("datum") || userInput.Contains("tag"))
+                return $"Heute ist der {DateTime.Now:dddd, dd. MMMM yyyy}.";
+
+            return "Entschuldigung, ich verstehe das nicht. Könnten Sie das bitte klären?";
+        }
+
+        /// <summary>
+        /// Retrieves the weather for the specified location asynchronously.
+        /// </summary>
+        /// <param name="location">The location for which to retrieve the weather.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the weather information.</returns>
+        public async Task<string> GetWeatherAsync(string location)
+        {
+            string requestUrl = $"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={apiKey}&units=metric&lang=de";
+
+            using HttpClient httpClient = new HttpClient();
+            try
+            {
+                string jsonResponse = await httpClient.GetStringAsync(requestUrl);
+                JObject weatherResponse = JObject.Parse(jsonResponse);
+
+                string weatherDescription = (string)weatherResponse.SelectToken("weather[0].description");
+                double temperature = (double)weatherResponse.SelectToken("main.temp");
+
+                return $"Das Wetter in {location} ist aktuell {weatherDescription} mit einer Temperatur von {temperature}°C.";
+            }
+            catch (Exception)
+            {
+                return "Es tut mir leid, ich konnte das Wetter für diesen Ort nicht abrufen. Bitte versuchen Sie es später noch einmal oder überprüfen Sie den Ort.";
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,112 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
+using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Configuration;
+using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace chatbot_application
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Represents the main window of the application.
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BotEngine botEngine;
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            botEngine = new BotEngine(configuration["OpenWeatherMapApiKey"]);
         }
+
+        /// <summary>
+        /// Handles the KeyDown event of the UserInput control.
+        /// </summary>
+        private void UserInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the SendButton control.
+        /// </summary>
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+        }
+
+        /// <summary>
+        /// Sends the user message and receives a response from the bot.
+        /// </summary>
+        private async void SendMessage()
+        {
+            string userInput = UserInput.Text;
+            UserInput.Clear();
+            ChatHistory.Items.Add(new UserMessage { Text = userInput });
+
+            string botResponse;
+            if (botEngine.IsWaitingForLocation)
+            {
+                botResponse = await botEngine.GetWeatherAsync(userInput);
+                botEngine.IsWaitingForLocation = false;
+            }
+            else
+            {
+                botResponse = botEngine.GetBotResponse(userInput);
+            }
+
+            ChatHistory.Items.Add(new BotMessage { Text = botResponse });
+
+            // Scroll to latest message
+            if (VisualTreeHelper.GetChildrenCount(ChatHistory) > 0)
+            {
+                var border = (Border)VisualTreeHelper.GetChild(ChatHistory, 0);
+                var scrollViewer = (ScrollViewer)VisualTreeHelper.GetChild(border, 0);
+                scrollViewer.ScrollToEnd();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Represents a user message.
+    /// </summary>
+    public class UserMessage
+    {
+        /// <summary>
+        /// Gets or sets the text of the user message.
+        /// </summary>
+        public string Text { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a bot message.
+    /// </summary>
+    public class BotMessage
+    {
+        /// <summary>
+        /// Gets or sets the text of the bot message.
+        /// </summary>
+        public string Text { get; set; }
     }
 }
